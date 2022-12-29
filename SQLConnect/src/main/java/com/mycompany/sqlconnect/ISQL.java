921a1715c95ca2b2ca08ac1b7cbf8ad2eaf44e88
Nikolay <Student@Student02>
2022-12-29 20:07:23 +0700

    29.12.22

--- SQLConnect/src/main/java/com/mycompany/sqlconnect/ISQL.java
+++ SQLConnect/src/main/java/com/mycompany/sqlconnect/ISQL.java
@@ -5,6 +5,9 @@
 package com.mycompany.sqlconnect;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  *
@@ -12,4 +15,20 @@ import java.sql.Connection;
  */
 public interface ISQL extends AutoCloseable{
     boolean connect(String url, String username, String password);
+    
+    boolean create(String tableName, String[] columns) throws SQLException, Exception;
+    
+    int insert(String tableName, String[][] rows);
+
+    int updateRows(String tableName, String column, String conditionColumn, String[] conditions, String[] values);
+    
+    boolean updateColumns(String tableName, String[] columns, String[] values, String condition);
+    
+    public LinkedList<String> select(String tableName,String[] columns);
+    
+    boolean delete(String tableName);
+    
+    int deleteRows(String tableName, String condition);
+    
+    boolean addColumn(String tableName, String column); //column = "name TEXT NOT NULL"
 }
--- SQLConnect/src/main/java/com/mycompany/sqlconnect/SQLConnect.java
+++ SQLConnect/src/main/java/com/mycompany/sqlconnect/SQLConnect.java
@@ -13,8 +13,21 @@ public class SQLConnect {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        ConnectSQLClass con = ConnectSQLClass.getinstance();
+        var url = "jdbc:postgresql://localhost:5432/postgres";
+        var user = "postgres";
+        var pass = "postgres";
+        var name= "my_table";
+        String[][] rows =  {{"25","'rtyy'","'ert'"},{"34","'tgtg'","'34'"},{"432","'fdgdf'","'dfgdf'"}};
         
-     ConnectSQLClass con=ConnectSQLClass.getinstance();
-     con.connect("jdbc:postgresql://localhost:5432/postgres", "postgres", "mctoooo");
+        con.connect(url, user, pass);
+        var arr = new String[3];
+        arr[0] = "v_id int,PRIMARY KEY";
+        arr[1] = "v_name text";
+        arr[2] = "potato varchar(10)";
+        if(con.create(name, arr)) System.out.print("YES");
+        if(con.insert(name, rows)!=-1) System.out.print("INSERTED");
+       
     }
+    
 }
